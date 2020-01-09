676290f03611c2db333ef132760ef153a86082a9
Nafis Fuad <fuadbd14@gmail.com>
2020-01-09 13:57:55 +0600

    Update manage page

--- pages/manage.tsx
+++ pages/manage.tsx
@@ -1,6 +1,46 @@
 import * as React from 'react'
+import { FormContext, useForm } from 'react-hook-form'
+import { findLinkByAlias, deleteLinkAlias } from '../graphql/api'
 
 // custom imports
 import Layout from '../components/Layout'
+import Header from '../components/Header'
+import Form from '../components/Form'
+import Error from '../components/Error'
 
-export default () => <Layout>whatever</Layout>
+export default () => {
+  const [errorMessage, setErrorMessage] = React.useState('')
+  const methods = useForm()
+
+  const onSubmit = async data => {
+    setErrorMessage('')
+    const alias = data.inputURL.slice(data.inputURL.length - 5)
+
+    try {
+      const res = await findLinkByAlias(alias)
+      if (!res.errors) {
+        const deleteRes = await deleteLinkAlias(res.data.findLinkByAlias._id)
+        if (!deleteRes.errors) setErrorMessage('')
+      }
+
+      setErrorMessage('The URL was not found in our Database')
+    } catch (error) {
+      setErrorMessage(
+        'There was an error while deleting the alias. Please try again later'
+      )
+    }
+  }
+
+  return (
+    <Layout title="Delete Alias">
+      <Header
+        heading="Want to Delete Alias?"
+        text="Put the shortened URL in the box:"
+      />
+      <FormContext {...methods}>
+        <Form onSubmit={onSubmit} action="delete!" />
+      </FormContext>
+      <Error>{errorMessage}</Error>
+    </Layout>
+  )
+}
